5f2992a7e5a09495817ec2c3cd32288ec84cc0f9
rhinochen <126566329+rhinochen@users.noreply.github.com>
2026-09-14 21:58:17 +0800

    Restore minimal override

--- override.js
+++ override.js
@@ -1,1 +1,3 @@
-console.log('weekly override ready');
+getReportMonthIndex = () => 7;
+applyManualReportOverrides = () => { rows[8][10] = 82000; rows[8][17] = 1270000; rows[8][21] = Math.max(Number(rows[8][21]) || 0, 82000); };
+renderDashboard();
