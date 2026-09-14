a628043b3f0b6e528f68ff666acd00bebb7283d2
rhinochen <126566329+rhinochen@users.noreply.github.com>
2026-09-14 22:15:19 +0800

    Fix September total revenue override

--- override.js
+++ override.js
@@ -9,8 +9,8 @@ renderLineChart = function renderLineChartWithoutCasesAnnotation(id, previous, c
 applyManualReportOverrides = () => {
   rows[8][10] = 82000;
   rows[8][17] = 1270000;
-  rows[8][21] = Math.max(Number(rows[8][21]) || 0, 82000);
+  rows[8][21] = 338400;
 };
 
 renderDashboard();
-status('週報已切換為 2026/09/14 的 9 月目前業績，並移除件數圖表註記');
+status('週報已切換為 2026/09/14 的 9 月目前業績，總收入修正為 $338,400，並移除件數圖表註記');
