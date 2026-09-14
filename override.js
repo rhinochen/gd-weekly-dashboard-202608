bf07ddcde813377a7753948a6ad7e3a2aa938440
rhinochen <126566329+rhinochen@users.noreply.github.com>
2026-09-14 22:07:57 +0800

    Switch first slides to September report

--- override.js
+++ override.js
@@ -1,4 +1,8 @@
-getReportMonthIndex = () => 7;
-applyManualReportOverrides = () => { rows[8][10] = 82000; rows[8][17] = 1270000; rows[8][21] = Math.max(Number(rows[8][21]) || 0, 82000); };
+getReportMonthIndex = () => 8;
+applyManualReportOverrides = () => {
+  rows[8][10] = 82000;
+  rows[8][17] = 1270000;
+  rows[8][21] = Math.max(Number(rows[8][21]) || 0, 82000);
+};
 renderDashboard();
-status('週報已更新至 2026/09/14');
+status('週報已切換為 2026/09/14 的 9 月目前業績');
