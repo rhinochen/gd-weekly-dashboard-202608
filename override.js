c9186cfe8914cf101c6c14a13ca5220260083f1d
rhinochen <126566329+rhinochen@users.noreply.github.com>
2026-09-14 22:00:31 +0800

    Try small status patch

--- override.js
+++ override.js
@@ -1,3 +1,4 @@
 getReportMonthIndex = () => 7;
 applyManualReportOverrides = () => { rows[8][10] = 82000; rows[8][17] = 1270000; rows[8][21] = Math.max(Number(rows[8][21]) || 0, 82000); };
 renderDashboard();
+status('週報已更新至 2026/09/14');
